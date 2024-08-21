161012ca38cf09074ae7613902e451ce18e056c4
batugkocak <batugkocak35@gmail.com>
2024-08-21 09:00:08 +0300

    feat: Add Autofac for IoC Container

--- Northwind.API/Program.cs
+++ Northwind.API/Program.cs
@@ -1,7 +1,6 @@
-using Northwind.Business.Abstract;
-using Northwind.Business.Concrete;
-using Northwind.DataAccess.Abstract;
-using Northwind.DataAccess.Concrete.EntityFramework;
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using Northwind.Business.Dependency_Resolvers;
 
 namespace Northwind.API
 {
@@ -11,11 +10,20 @@ namespace Northwind.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+            builder.Host.ConfigureContainer<ContainerBuilder>(bldr =>
+            {
+
+                bldr.RegisterModule(new AutofacBusinessModule());
+            });
+
             builder.Services.AddControllers();
       
-            builder.Services.AddSingleton<IProductService, ProductManager>();
+            // Changed to Autofac : 
+            //builder.Services.AddSingleton<IProductService, ProductManager>();
                                                                               
-            builder.Services.AddSingleton<IProductDal, EfProductDal>();  
+            //builder.Services.AddSingleton<IProductDal, EfProductDal>();  
 
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
---
+++ Northwind.Business/Dependency Resolvers/Autofac/AutofacBusinessModule.cs.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using Northwind.Business.Abstract;
+using Northwind.Business.Concrete;
+using Northwind.DataAccess.Abstract;
+using Northwind.DataAccess.Concrete.EntityFramework;
+
+namespace Northwind.Business.Dependency_Resolvers
+{
+    public class AutofacBusinessModule : Module 
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // Product: 
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+        }
+    }
+}
